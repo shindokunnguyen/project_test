819a32e66931d39440ea8fa01144f32e1fcee568
selkymy <nguyenmanhhung0101@gmail.com>
2017-11-23 01:13:26 +0700

    first commit

---
+++ database/migrations/2017_11_22_144224_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('art_id');
+            $table->string('art_title');
+            $table->string('art_content')->nullable();
+            $table->string('art_author')->nullable();
+            $table->integer('art_is_deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
---
+++ database/migrations/2017_11_22_144251_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('comment_id');
+            $table->string('comment_content');
+            $table->string('comment_art_id');
+            $table->string('comment_user_id');
+            $table->integer('comment_is_deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
