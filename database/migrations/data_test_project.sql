0d776de47c58f30d27ec6c28ccd9d5f6ed60f6e6
selkymy <nguyenmanhhung0101@gmail.com>
2017-11-26 23:17:34 +0700

    commit file data simple test

---
+++ database/migrations/data_test_project.sql
@@ -0,0 +1,52 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Records of articles
+-- ----------------------------
+INSERT INTO `articles` VALUES ('1', 'hungnmtest_1', '<p>Robinho từng c&oacute; qu&atilde;ng thời k&igrave; v&agrave;ng son trong m&agrave;u &aacute;o Real Madrid. Anh c&ugrave;ng Kền Kền Trắng 2 m&ugrave;a li&ecirc;n tiếp l&ecirc;n đỉnh&nbsp;<a href=\"http://bongdaplus.vn/la-liga-tags.html\" target=\"_blank\">La Liga</a>&nbsp;(2006/07, 2007/08) trước khi lần lượt cập bến Man City, Milan, Guangzhou Evergrande v&agrave; giờ l&agrave; Atletico Mineiro.</p>\r\n\r\n<p>Ở cấp độ ĐTQG, Robinho sở hữu tr&ograve;n 100 lần kho&aacute;c &aacute;o ĐT Brazil với 28 b&agrave;n thắng. Dấu ấn lớn nhất của anh l&agrave; gi&uacute;p đội b&oacute;ng xứ Samba v&ocirc; địch Copa America 2007 v&agrave; 2 lần l&ecirc;n ng&ocirc;i ở Confed Cup (2005, 2009).&nbsp;</p>\r\n', '1', '0', '2017-11-24 15:42:58', '2017-11-24 16:20:09', 'nguyenmanhhung');
+INSERT INTO `articles` VALUES ('2', 'article 2', '<p>article 2article 2article 2article 2article 2</p>\r\n\r\n<p>article 2article 2article 2article 2</p>\r\n\r\n<p>article 2article 2</p>\r\n\r\n<p>article 2article 2</p>\r\n', '1', '0', '2017-11-26 14:41:40', '2017-11-26 14:41:40', 'nguyenmanhhung');
+INSERT INTO `articles` VALUES ('3', 'article 3', '<p>article 3article 3article 3article 3article 3article 3article 3article 3</p>\r\n\r\n<p>article 3article 3article 3article 3article 3article 3article 3article 3article 3article 3</p>\r\n\r\n<p>article 3article 3article 3article 3article 3article 3article 3article 3</p>\r\n\r\n<p>article 3article 3article 3article 3article 3</p>\r\n\r\n<table border=\"1\" cellpadding=\"1\" cellspacing=\"1\" style=\"height:100px; width:500px\" summary=\"hungnm\">\r\n	<caption>table test</caption>\r\n	<tbody>\r\n		<tr>\r\n			<td>1</td>\r\n			<td>1</td>\r\n		</tr>\r\n		<tr>\r\n			<td>2</td>\r\n			<td>2</td>\r\n		</tr>\r\n		<tr>\r\n			<td>3</td>\r\n			<td>3</td>\r\n		</tr>\r\n	</tbody>\r\n</table>\r\n\r\n<p>&nbsp;</p>\r\n', '1', '0', '2017-11-26 14:46:40', '2017-11-26 14:46:40', 'nguyenmanhhung');
+INSERT INTO `articles` VALUES ('4', 'article 4', '<p>article 4article 4article 4article 4</p>\r\n\r\n<p>article 4article 4article 4article 4article 4article 4</p>\r\n\r\n<p><img alt=\"tuyen viet nam\" src=\"http://static.thanhnien.com.vn/Uploaded/sontung/2015_11_18/vn-iraq25_GACG.jpg\" style=\"float:left; height:300px; width:450px\" /></p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>article 4article 4article 4article 4</p>\r\n', '1', '0', '2017-11-26 14:49:27', '2017-11-26 14:49:27', 'nguyenmanhhung');
+INSERT INTO `articles` VALUES ('5', 'artilce 5', '<p>artilce 5</p>\r\n\r\n<p>artilce 5arti<strong>lce 5artilce 5artilce 5artilce 5art</strong>ilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>a<em>rtilce 5artilce 5artilce 5</em></p>\r\n\r\n<p>artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5artilce 5</p>\r\n', '1', '0', '2017-11-26 14:50:04', '2017-11-26 14:50:04', 'nguyenmanhhung');
+INSERT INTO `articles` VALUES ('6', 'article 6', '<p>article 6article 6article 6article 6</p>\r\n\r\n<p>article 6</p>\r\n\r\n<p>article 6article 6article 6article 6article 6article 6article 6</p>\r\n\r\n<p>article 6article 6</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>article 6article 6article 6</p>\r\n\r\n<p>article 6</p>\r\n', '1', '0', '2017-11-26 14:50:40', '2017-11-26 14:50:40', 'nguyenmanhhung');
+INSERT INTO `articles` VALUES ('7', 'article 7 ', '<p>article 7&nbsp;article 7&nbsp;article 7&nbsp;</p>\r\n\r\n<p>article 7&nbsp;</p>\r\n', '1', '0', '2017-11-26 14:50:59', '2017-11-26 14:50:59', 'nguyenmanhhung');
+INSERT INTO `articles` VALUES ('8', 'article 8', '<p>article 8article 8article 8article 8article 8article 8article 8article 8</p>\r\n\r\n<p>article 8article 8article 8article 8article 8article 8article 8article 8article 8article 8article 8article 8article 8article 8</p>\r\n\r\n<p>article 8</p>\r\n', '1', '0', '2017-11-26 14:51:15', '2017-11-26 14:51:15', 'nguyenmanhhung');
+INSERT INTO `articles` VALUES ('9', 'article 9', '<p>article 9article 9</p>\r\n', '1', '0', '2017-11-26 14:51:24', '2017-11-26 14:51:24', 'nguyenmanhhung');
+INSERT INTO `articles` VALUES ('10', 'article 10', '<p>article 10article 10article 10article 10article 10</p>\r\n\r\n<p>article 10article 10article 10article 10</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>article 10article 10article 10article 10</p>\r\n\r\n<p>article 10article 10article 10</p>\r\n', '1', '0', '2017-11-26 14:51:38', '2017-11-26 14:51:38', 'nguyenmanhhung');
+INSERT INTO `articles` VALUES ('11', 'article 11', '<p>article 11article 11</p>\r\n', '2', '0', '2017-11-26 14:52:08', '2017-11-26 14:52:08', 'doanlieu');
+INSERT INTO `articles` VALUES ('12', 'article 12', '<p>article 11article 11article 11article 11article 11article 11article 11article 11article 11article 11article 11article 11article 11article 11article 112</p>\r\n\r\n<p>article 12</p>\r\n\r\n<p>article 12article 12article 12article 12article 12article 12article 12article 12article 12article 12article 12article 12</p>\r\n\r\n<p>article 12article 12</p>\r\n', '2', '0', '2017-11-26 14:52:28', '2017-11-26 14:52:28', 'doanlieu');
+
+-- ----------------------------
+-- Records of comments
+-- ----------------------------
+INSERT INTO `comments` VALUES ('1', 'hungnm', '1', '1', '0', '2017-11-26 22:32:09', '2017-11-25 17:02:45', 'hungnm');
+INSERT INTO `comments` VALUES ('2', 'hungnm first comment', '1', '1', '0', '2017-11-25 17:02:33', '2017-11-25 17:02:33', 'hungnm');
+INSERT INTO `comments` VALUES ('3', 'hehhe \ndey la comment thu 2', '1', '1', '0', '2017-11-25 17:06:49', '2017-11-25 17:06:49', 'hungnm');
+INSERT INTO `comments` VALUES ('4', 'dey la lan thu 3 nhe\' !!!!', '1', '1', '0', '2017-11-25 17:07:39', '2017-11-25 17:07:39', 'hungnm');
+INSERT INTO `comments` VALUES ('5', 'dey la lan thu 3 nhe\' !!!!', '1', '1', '0', '2017-11-25 17:07:48', '2017-11-25 17:07:48', 'hungnm');
+INSERT INTO `comments` VALUES ('6', 'lan cuoi\' nhe\' =))))', '1', '1', '0', '2017-11-25 17:55:49', '2017-11-25 17:55:49', 'hungnm');
+INSERT INTO `comments` VALUES ('7', '11111111111', '1', '1', '0', '2017-11-25 17:56:51', '2017-11-25 17:56:51', 'hungnm');
+INSERT INTO `comments` VALUES ('8', 'vcbcbb', '1', '1', '0', '2017-11-25 17:57:54', '2017-11-25 17:57:54', 'hungnm');
+INSERT INTO `comments` VALUES ('9', 'not phat nay', '1', '1', '0', '2017-11-25 17:58:26', '2017-11-25 17:58:26', 'hungnm');
+INSERT INTO `comments` VALUES ('10', 'doan lieu comment ', '1', '2', '0', '2017-11-26 12:54:38', '2017-11-26 12:54:38', 'doanlieu');
+INSERT INTO `comments` VALUES ('11', 'unlike roi', '1', '2', '0', '2017-11-26 14:31:07', '2017-11-26 14:31:07', 'doanlieu');
+INSERT INTO `comments` VALUES ('12', 'comment test thu 1 cai\' xem nao`\ndfg\ndfg\ndf\ng\ndfgd', '1', '3', '0', '2017-11-26 15:32:49', '2017-11-26 15:32:49', 'tenten');
+INSERT INTO `comments` VALUES ('13', 'comment test thu 1 cai\' xem nao`\ndfg\ndfg\ndf\ng\ndfgd', '1', '3', '0', '2017-11-26 15:34:05', '2017-11-26 15:34:05', 'tenten');
+INSERT INTO `comments` VALUES ('14', '1\nkhakka\nkahkah', '1', '3', '0', '2017-11-26 15:35:09', '2017-11-26 15:35:09', 'tenten');
+
+
+-- ----------------------------
+-- Records of likes
+-- ----------------------------
+INSERT INTO `likes` VALUES ('4', '1', '1', '2017-11-25 15:50:29', '2017-11-25 15:50:29');
+INSERT INTO `likes` VALUES ('6', '12', '2', '2017-11-26 14:52:48', '2017-11-26 14:52:48');
+INSERT INTO `likes` VALUES ('7', '11', '2', '2017-11-26 14:52:56', '2017-11-26 14:52:56');
+INSERT INTO `likes` VALUES ('8', '1', '3', '2017-11-26 15:34:59', '2017-11-26 15:34:59');
+
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES ('1', 'nguyenmanhhung', 'hungnm@gmail.com', '$2y$10$6wmwvSTXwtittuxB02xdcOY0r4MApuLwvMiiwCdO149TYns0kuwNK', 'Fkm1Tz3IXo6uJlrH5Vf890MFOsn0CAZKDtAnAo3i5EDP8WhUWuAShc1m4ot0', '2017-11-24 15:42:39', '2017-11-24 15:42:39', '2017-11-30', '0');
+INSERT INTO `users` VALUES ('2', 'doanlieu', 'doanlieu3112@gmail.com', '$2y$10$QzpBeQBnniStafV934zlvOqAn1Hv1E8r4DU3yBFywXhVflB6SGZtO', '6fv2IKLz4tp26HHvjn7wPu9UxgdI8PplUNKkqjLU0X14BCIXdp2dJCowN7DR', '2017-11-26 12:54:21', '2017-11-26 12:54:21', '2017-11-30', '0');
+INSERT INTO `users` VALUES ('3', 'tenten', 'ten@hungnm.com', '$2y$10$.bWVMj.6LSGMtALC/pgoFeAu0BM3ePsCcqEZk9YbddcH38cM6Ol5K', 'BYb2KzALbO7WO89QjbdMSAhyKuuhmxasNe5OhgZjFbfhdaQlrvOL8CnUs1E0', '2017-11-26 15:15:20', '2017-11-26 15:15:20', '2017-11-02', '0');
